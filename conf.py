c161ac80a50c86a84f735acdf534fdc130426e85
Benoît Clouet <benoit.clouet@gmail.com>
2025-03-10 09:51:45 +0100

    Updated license

--- conf.py
+++ conf.py
@@ -977,7 +977,26 @@ FEED_LINKS_APPEND_QUERY = False
 
 # A HTML fragment describing the license, for the sidebar.
 # (translatable)
-LICENSE = "https://creativecommons.org/licenses/by-sa/4.0/"
+LICENSE = """<p xmlns:cc="http://creativecommons.org/ns#" 
+                xmlns:dct="http://purl.org/dc/terms/">
+                <a  property="dct:title" 
+                    rel="cc:attributionURL" 
+                    href="https://blog.bclouet.eu">A blog</a>
+                by <span property="cc:attributionName">Benoît Clouet</span>, licensed under 
+                <a  href="https://creativecommons.org/licenses/by-sa/4.0/?ref=chooser-v1" 
+                    target="_blank" rel="license noopener noreferrer" 
+                    style="display:inline-block;">CC BY-SA 4.0
+                    <img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" 
+                    src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1" 
+                    alt="">
+                        <img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" 
+                              src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1" 
+                              alt="">
+                              <img  style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" 
+                                    src="https://mirrors.creativecommons.org/presskit/icons/sa.svg?ref=chooser-v1" 
+                                    alt="">
+                </a>
+            </p>"""
 # I recommend using the Creative Commons' wizard:
 # https://creativecommons.org/choose/
 # LICENSE = """
